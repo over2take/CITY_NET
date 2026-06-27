dcb77a2846ab92d95367a8e2f6940cf2d30ac821
Developer <dev@example.com>
2026-06-27 18:03:42 -0500

    Clean up unused scratch and patch scripts

--- frontend/patch_raycast.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/App.tsx', 'utf8');
-
-const badRaycast = /raycast=\{isDragging \? \(\) => null : undefined\}/g;
-code = code.replace(badRaycast, '');
-
-fs.writeFileSync('src/App.tsx', code);
-console.log("Removed broken raycast prop mutation");
