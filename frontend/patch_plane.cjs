dc20d78d5d918dd8b8c52c1322ef12302aa6cf14
Developer <dev@example.com>
2026-06-27 18:03:42 -0500

    Clean up unused scratch and patch scripts

--- frontend/patch_plane.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/App.tsx', 'utf8');
-
-const regexPlane = /\{isLocalDragging && \(\s*<mesh\s*rotation=\{\[-Math\.PI \/ 2, 0, 0\]\}\s*position=\{\[0, -visualPos\.current\.y, 0\]\}\s*scale=\{\[10000, 10000, 1\]\}\s*onPointerMove=\{handlePointerMove\}\s*onPointerUp=\{handlePointerUp\}\s*>\s*<planeGeometry \/>\s*<meshBasicMaterial visible=\{false\} \/>\s*<\/mesh>\s*\)\}/g;
-
-code = code.replace(regexPlane, '');
-
-fs.writeFileSync('src/App.tsx', code);
-console.log("Deleted infinite planes");
